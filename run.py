2ee24d299e3e59b6df05044e4de55ff9463c9971
Marijoke <mariam.adamu@hotmail.co.uk>
2021-06-06 11:34:47 +0000

    I used some of the info from previous modules to create a ttempleate for my project. This includes adding a nav

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+#This links to the html pages to avoid writing html in this doc
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+    @app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+"""
+(__main__) is the name of the default module in Python.
+the app will be run using the statements in (__main__)
+"""
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+
+
