13db2ca0bc446b0f7bf16fc1de34bd548bfdb12d
Marijoke <mariam.adamu@hotmail.co.uk>
2021-06-07 20:22:52 +0000

    errors in env file resolved. Secret key had charaters that was breaking the code

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-    import env.py
+    import env
 
 
 app = Flask(__name__)
 
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", None)
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
 mongo = PyMongo(app)
 
 
+"""
 @app.route("/")
 def index():
     return render_template("index.html")
+"""
 
 
 @app.route("/get_books")
-def get_book():
+def get_books():
     books = mongo.db.books.find()
-    return render_template("post.html", books=books)
+    return render_template("books.html", books=books)
 
 
+"""
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -38,12 +39,9 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+"""
 
 
-"""
-(__main__) is the name of the default module in Python.
-the app will be run using the statements in (__main__)
-"""
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
