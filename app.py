1fdafc54fde17b6efbfedb7292dbb3e4fe9e2eff
Marijoke <mariam.adamu@hotmail.co.uk>
2021-06-08 18:44:49 +0000

    Registration form complete, data encrypted and is recieved by Mongo successfully

--- app.py
+++ app.py
@@ -39,9 +39,27 @@ def books():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
-    
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
