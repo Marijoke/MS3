94f254f753590470077b20bc28f23553c123afe2
Marijoke <mariam.adamu@hotmail.co.uk>
2021-06-08 19:21:42 +0000

    Log out button included and only shown to session users.

--- app.py
+++ app.py
@@ -92,6 +92,14 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+    
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
